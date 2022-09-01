2d034f2d0805308f4a822d8292cd299dff7a9b46
leekimchon <leekimchon01@gmail.com>
2022-09-01 17:11:03 +0700

    repository service pattern

---
+++ app/Repositories/RepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Repositories;
+
+interface RepositoryInterface{
+    public function all();
+    public function find($id);
+    public function create($data);
+    public function update($id, $data);
+    public function delete($id);
+}
---
+++ app/Services/Book/BookServiceInterface.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Services\Book;
+
+use App\Services\ServiceInterface;
+
+interface BookServiceInterface extends ServiceInterface{
+    
+}
