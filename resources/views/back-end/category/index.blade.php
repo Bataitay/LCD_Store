b868ac17c0d3a90a6dcda23a3a82e1f5cb0ca6db
Bataitay <duongmg99@gmail.com>
2022-09-02 09:16:13 +0700

    index category

--- resources/views/back-end/category/index.blade.php
+++ resources/views/back-end/category/index.blade.php
@@ -1,4 +1,62 @@
 @extends('back-end.master')
 @section('content')
+<style>
+    .title_cate {
+        margin-left: 30px;
+    }
+</style>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row ">
+                            <div class="col-md-4">
+                                <div class="md-3">
+                                    <h2 for="example-text-input" class="form-label">Tên nhà cung cấp</h2>
+                                </div>
+                            </div>
+                            <div class="col-md-8">
+                                <h2></h2>
+                            </div>
+                            <div class="col-md-4 ">
+                                <div class="md-3 title_cate">
+                                    <a href="" class="btn btn-secondary btn-rounded waves-effect waves-light ">
+                                    <i class="mdi mdi-plus-circle addeventmore "></i>
+                                    Add Category</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-body">
 
+                            <table
+                                class="table table-bordered dt-responsive nowrap text-center align-middle dataTable no-footer dtr-inline"
+                                style="border-color: #ddd; border-spacing: 0; width: 100%;">
+                                <thead>
+                                    <tr>
+                                        <th width="17%">Name</th>
+                                        <th>Quantity</th>
+                                        <th>Thao tác</th>
+                                    </tr>
+                                </thead>
+
+                                <tbody id="addRow" class="addRow">
+
+                                </tbody>
+
+                                <tbody>
+                                    <tr>
+                                        <td></td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
