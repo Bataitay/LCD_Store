53bc7a2fb6c9ba0564710a428ed75a99e2bd2269
leekimchon <leekimchon01@gmail.com>
2022-09-06 19:58:32 +0700

    change route back

--- resources/views/back-end/role/sorfDelete.blade.php
+++ resources/views/back-end/role/sorfDelete.blade.php
@@ -16,7 +16,7 @@
                             </div>
                             <div class="col-md-12 d-flex">
                                 <div class="md-3 title_cate">
-                                    <a href="{{ route('role.index') }}"
+                                    <a href="{{ url()->previous() }}"
                                         class="btn btn-danger btn-rounded waves-effect waves-light ">
                                         <i class=" fas fa-reply-all"></i>
                                         All Roles</a>
