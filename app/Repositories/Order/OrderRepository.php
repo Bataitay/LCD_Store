ace41e734eaf508089c07671ed57aea7db930a8e
leekimchon <leekimchon01@gmail.com>
2022-09-07 09:48:36 +0700

    update search order

--- app/Repositories/Order/OrderRepository.php
+++ app/Repositories/Order/OrderRepository.php
@@ -12,7 +12,7 @@ class OrderRepository extends BaseRepository implements OrderRepositoryInterface
         $orders = $this->model->latest()->paginate(1);
         if(isset($request->search)){
             $orders = $this->model
-            ->select('*', 'orders.id as id')
+            ->select('*', 'orders.id as id', 'orders.created_at as created_at', 'orders.updated_at as updated_at', 'orders.deleted_at as deleted_at')
             ->join('customers', 'orders.customer_id', '=', 'customers.id')
             ->where('orders.id', 'LIKE', '%'.$request->search.'%')
             ->orWhere('customers.name', 'LIKE', '%'.$request->search.'%')
