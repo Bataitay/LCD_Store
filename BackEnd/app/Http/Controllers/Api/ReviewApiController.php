87238c1c3add48679a34d4fc335d51d37de44e9c
Sa-luy <jockersaluy@gmail.com>
2022-09-12 16:47:32 +0700

    complete api_review

--- BackEnd/app/Http/Controllers/Api/ReviewApiController.php
+++ BackEnd/app/Http/Controllers/Api/ReviewApiController.php
@@ -19,41 +19,54 @@ class ReviewApiController extends Controller
 
         try {
             $reviews =  $this->reviewService->all($request);
-            $statusCode= 200 ;
-        return response()->json($reviews,$statusCode);
+            $statusCode = 200;
+            return response()->json($reviews, $statusCode);
         } catch (\Exception $e) {
             Log::error('errors' . $e->getMessage() . 'getLine' . $e->getLine());
-            $reviews =[];
-            $statusCode= 500 ;
-            return response()->json($reviews,$statusCode);
+            $reviews = [];
+            $statusCode = 500;
+            return response()->json($reviews, $statusCode);
         }
-
     }
     public function store(Request $request)
     {
         try {
-           $review=  $this->reviewService->create($request->all());
-           $statusCode= 200 ;
-           return response()->json($review,$statusCode);
-        }  catch (\Exception $e) {
+            $review =  $this->reviewService->create($request->all());
+            $statusCode = 200;
+            return response()->json($review, $statusCode);
+        } catch (\Exception $e) {
             Log::error('errors' . $e->getMessage() . 'getLine' . $e->getLine());
-            $statusCode= 500 ;
-            $review=  [];
-            return response()->json($review,$statusCode);
+            $statusCode = 500;
+            $review =  [];
+            return response()->json($review, $statusCode);
         }
     }
     public function update($id, Request $request)
     {
-        try{
-        $newReview=  $request->all();
-        $this->reviewService->update($id, $newReview);
-        $statusCode= 200 ;
-        return response()->json($newReview,$statusCode);
-     }  catch (\Exception $e) {
-         Log::error('errors' . $e->getMessage() . 'getLine' . $e->getLine());
-         $statusCode= 500 ;
-         $newReview=  [];
-         return response()->json($newReview,$statusCode);
-     }
+        try {
+            $newReview =  $request->all();
+            $this->reviewService->update($id, $newReview);
+            $statusCode = 200;
+            return response()->json($newReview, $statusCode);
+        } catch (\Exception $e) {
+            Log::error('errors' . $e->getMessage() . 'getLine' . $e->getLine());
+            $statusCode = 500;
+            $newReview =  [];
+            return response()->json($newReview, $statusCode);
+        }
+    }
+    public function destroy($id)
+    {
+        try {
+            $review =  $this->reviewService->find($id);
+            $this->reviewService->delete($id);
+            $statusCode = 200;
+            return response()->json($review, $statusCode);
+        } catch (\Exception $e) {
+            Log::error('errors' . $e->getMessage() . 'getLine' . $e->getLine());
+            $statusCode = 500;
+            $newReview =  [];
+            return response()->json($newReview, $statusCode);
+        }
     }
 }
