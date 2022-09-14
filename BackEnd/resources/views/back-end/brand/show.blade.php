b5c0d4ea6359f04708028a80f4a7e0c46ababa32
Sa-luy <jockersaluy@gmail.com>
2022-09-14 16:03:59 +0700

    update view brand review customer

--- BackEnd/resources/views/back-end/brand/show.blade.php
+++ BackEnd/resources/views/back-end/brand/show.blade.php
@@ -1,9 +1,18 @@
 @extends('back-end.master')
 @section('content')
     <div class="container-fluid">
-        <p>Brand Name :</p>
-        <h4>{{$brand->name}}</h4>
-        <p>Brand Logo: </p>
-        <img src="{{ asset($brand->logo) }}" alt="" width="250px">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <p>Brand Name :</p>
+                        <h4>{{ $brand->name }}</h4>
+                        <p>Brand Logo: </p>
+                        <img src="{{ asset($brand->logo) }}" alt="" width="250px">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
 @endsection
