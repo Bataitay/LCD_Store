60af12901fb6d0ba306ef56b0747e661107853c8
leekimchon <leekimchon01@gmail.com>
2022-09-19 14:35:52 +0700

    validate view check order

--- FrontEnd/src/app/shop/components/order-check.component.ts
+++ FrontEnd/src/app/shop/components/order-check.component.ts
@@ -9,6 +9,7 @@ import { OrderService } from '../service/order.service';
 })
 export class OrderCheckComponent implements OnInit {
     form!: FormGroup;
+    isEmptyOrder: boolean = false;
     constructor(
         private orderService: OrderService,
         private _router: Router
@@ -26,7 +27,7 @@ export class OrderCheckComponent implements OnInit {
         if (this.form.valid) {
             this.orderService.showOrder(this.form.value.orderId).subscribe(res => {
                 if (Object.keys(res).length === 0 && res.constructor === Object) {
-
+                    this.isEmptyOrder = true;
                 }else{
                     this._router.navigate(['order-detail', this.form.value.orderId]);
                 }
